9d4b97ed40c7ce4efbe52dbaf345f835b946aca1
NataliaJava <105055910+NataliaJava@users.noreply.github.com>
2024-02-20 18:45:33 +0200

    First files

---
+++ src/main/java/com/java/quizjavatgbot/QuizJavaTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.java.quizjavatgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizJavaTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizJavaTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/quizjavatgbot/QuizJavaTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.quizjavatgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizJavaTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
