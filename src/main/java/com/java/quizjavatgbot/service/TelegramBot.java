876f50e93e4108a363fc980a2d82a33568e00ea3
NataliaJava <105055910+NataliaJava@users.noreply.github.com>
2024-02-20 21:00:23 +0200

    Create method start.

--- src/main/java/com/java/quizjavatgbot/service/TelegramBot.java
+++ src/main/java/com/java/quizjavatgbot/service/TelegramBot.java
@@ -2,11 +2,14 @@ package com.java.quizjavatgbot.service;
 
 import com.java.quizjavatgbot.config.BotConfig;
 import com.java.quizjavatgbot.model.UserRepository;
+import com.vdurmont.emoji.EmojiParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @Component
@@ -33,6 +36,41 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            String firstNameChat = update.getMessage().getChat().getFirstName();
 
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, firstNameChat);
+                    break;
+            }
+        }
+
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+
+        String answer = EmojiParser.parseToUnicode("Hi, " + name + ", nice to meet you!" + " :blush:");
+        log.info("Replied to user " + name + " \"start\"");
+
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        executeMessage(message);
+    }
+
+    private void executeMessage(SendMessage message){
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
     }
 }
