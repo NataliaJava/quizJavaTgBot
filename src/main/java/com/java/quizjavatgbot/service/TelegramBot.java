e9948ac390e87178cff1d17e362804091e3909fe
NataliaJava <105055910+NataliaJava@users.noreply.github.com>
2024-02-20 21:29:17 +0200

    Create menu.

--- src/main/java/com/java/quizjavatgbot/service/TelegramBot.java
+++ src/main/java/com/java/quizjavatgbot/service/TelegramBot.java
@@ -7,9 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -19,9 +25,29 @@ public class TelegramBot extends TelegramLongPollingBot {
     private UserRepository userRepository;
 
     final BotConfig config;
+    String HELP_TEXT = "This bot is created to demonstrate Spring capabilities. \n\n" +
+            "You can execute commands from the main menu on the left or by typing a command: \n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /register to see a alert message about register in bot\n\n" +
+            "Type /help to see a this message again\n\n" +
+            "Type /aboutbot to see a message info about bot\n\n" +
+            "Type /mydata to see message with data about you\n\n" +
+            "Type /quiz to see a quiz\n\n";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/register", "register in bot"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/aboutbot", "show a message info about bot"));
+        listOfCommands.add(new BotCommand("/aboutnatta", "see message info about creator bot"));
+        listOfCommands.add(new BotCommand("/quiz", "show quiz"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -44,6 +70,9 @@ public class TelegramBot extends TelegramLongPollingBot {
             switch (messageText) {
                 case "/start":
                     startCommandReceived(chatId, firstNameChat);
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
                     break;
             }
         }
